82dd1a56cec79e37168ca61e3e5d5e94201e904e
Seol-hye <hs1507lll@gmail.com>
2026-06-04 16:14:47 +0900

    javascript 기초문법/변수/연산자/조건문

---
+++ javascript/basic/basic01.js
@@ -0,0 +1,47 @@
+
+// javascript
+
+console.log('basic01.js');
+console.log('text');
+
+// 변수 선언
+// var let const
+
+// 일반적인 변수 선언 : let
+// 상수 선언 : const
+let a = 10; //정수
+let b = 20.123; //실수
+let c = "안녕하세요"; //문자열
+let d = '작은따옴표';
+let e = true; //boolean
+let f = false;
+
+const g = 30;
+const h = "헬로우";
+
+a = 20;
+//g = 50; //const변수 (상수형) 변경 불가
+
+console.log('abc');
+console.log("abcd");
+console.log('작은 따옴표 "안녕" ㅎㅎ');
+console.log("큰따옴표 '안녕' ㅎㅎ");
+console.log("큰따옴표 '안녕' \"ㅎㅎ\" ");
+console.log(`백틱문자로 감싸면 "큰따옴표" '작은따옴표' 편함`);
+
+let x = 20;
+let y;
+
+console.log(x);
+console.log(y); //undefined 정의되지않음
+
+z = 50;
+console.log(z);
+
+//let z = 30;  불가
+var z = 30;
+console.log(z);
+var z = 20;
+console.log(z);
+
+
